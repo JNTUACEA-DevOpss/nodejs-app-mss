ef2f701abac8645c92138520bab5fb76e97b57f6
Deepika2516 <78471596+Deepika2516@users.noreply.github.com>
2021-03-09 19:24:50 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.179.124.93:9000/',
+  serverUrl: 'http://13.233.137.159:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.login': '6bd1aa49653c517120d373630d215975400ac325',
+	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
